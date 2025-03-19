b40ab92edd901186b49885cba9af58356cee1aa5
TacoConKvass <e_frun@o2.pl>
2025-03-19 11:11:44 +0100

    [Add] Telescope mappings

--- init.lua
+++ init.lua
@@ -4,9 +4,6 @@ vim.opt.cursorline = true
 vim.opt.breakindent = true
 vim.opt.scrolloff = 10
 
--- Save undo history
-vim.opt.undofile = true
-
 -- Set \ as map leader
 vim.g.mapleader = '\\'
 
@@ -57,7 +54,14 @@ return require('packer').startup(function(use)
 
 	use ({
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { {'nvim-lua/plenary.nvim'} },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		end
 	})
 
 	-- Automatically set up your configuration after cloning packer.nvim
