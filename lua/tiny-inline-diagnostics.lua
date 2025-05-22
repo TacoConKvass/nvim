d243f25130274415fdcc56954cc652f27b6c101a
TacoConKvass <e_frun@o2.pl>
2025-05-22 17:43:47 +0200

    [Setup] Inline errors

--- lua/tiny-inline-diagnostics.lua
+++ lua/tiny-inline-diagnostics.lua
@@ -1,5 +1,11 @@
 return {
     'rachartier/tiny-inline-diagnostic.nvim',
-    event = 'LspAttach',
-    priority = 1000 -- needs to be loaded in first
+    event = 'VeryLazy',
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+        require('tiny-inline-diagnostic').setup({
+            preset = 'powerline',
+        })
+        vim.diagnostic.config({ virtual_text = false })
+    end
 }
