6b64262128cded0f4f2d264484ae1d0a3c8ed8fe
TacoConKvass <e_frun@o2.pl>
2025-05-22 17:45:49 +0200

    [Change] Swap " for ', disable swapfile

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out, 'WarningMsg' },
+            { '\nPress any key to exit...' },
         }, true, {})
         vim.fn.getchar()
         os.exit(1)
@@ -26,22 +26,13 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.signcolumn = 'yes:1'
 
--- Set \ as map leader
+vim.opt.swapfile = false
+
 vim.g.mapleader = '\\'
 
--- LSP --
-vim.lsp.config('csharp_ls', {
-    cmd = { 'csharp-ls' },
-    filetypes = { 'cs' },
-    root_markers = { '.sln', '.git', '.csproj' },
-    init_options = {
-        AutomaticWorkspaceInit = true,
-    },
-})
-
 vim.diagnostic.config({
-	virtual_text = false,
     update_in_insert = true
 })
 
+-- << Plugins >> --
 require('lazy').setup('plugins')
